510826a541fbf4f1be94310177935688dc4c8b6d
David <david.tomecek1@seznam.cz>
2021-10-27 10:41:49 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spackeno-car-counter',
+    version='1.0',
+    author='David Tomecek',
+    author_email='david.tomecek1@seznam.cz',
+    packages=find_packages()
+)
