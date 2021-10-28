0685b28c774df0a3d3b18e8282f7a8b493bcccde
David <david.tomecek1@seznam.cz>
2021-10-29 00:33:08 +0200

    Added commentary in control module

--- spaceknow/control.py
+++ spaceknow/control.py
@@ -2,24 +2,40 @@ from spaceknow.api import TaskingObject
 from spaceknow.errors import TaskingException
 from spaceknow.models import TaskingStatus
 from time import sleep
-from typing import Callable, Iterable
+from typing import Callable
 
 
 class TaskingManager:
+    """Controls execution of TaskingObjects."""
     TASK_FAILED_ERROR = 'TASKING-FAILED'
     def __init__(self, logger: Callable[[str, int], None] = None) -> None:
+        """
+        Args:
+            logger (Callable[[str, int], None]): Logs status of a TaskingObject (status: str, time_untill_next _tep: int). Defaults to None.
+        """
         self.__logger = logger or (lambda s, i: None)
 
-    def wait_untill_completed(self, taskingObject: TaskingObject) -> Iterable:
-        status, wait_in_seconds = taskingObject.get_status()
+    def wait_untill_completed(self, tasking_object: TaskingObject):
+        """Waits untill the Tasking procedure is finished and returns the result
+
+        Args:
+            tasking_object (TaskingObject): [description]
+
+        Raises:
+            TaskingException: [description]
+
+        Returns:
+            Iterable: [description]
+        """
+        status, wait_in_seconds = tasking_object.get_status()
         if status in [TaskingStatus.PROCESSING, TaskingStatus.NEW]:
             self.__logger(status.name, wait_in_seconds)
             sleep(wait_in_seconds)
-            return self.wait_untill_completed(taskingObject)
+            return self.wait_untill_completed(tasking_object)
         elif status == TaskingStatus.FAILED:
             raise TaskingException(self.TASK_FAILED_ERROR,'Tasking failed unexpectedly.')
         self.__logger(status.name, wait_in_seconds)
-        return taskingObject.retrieve_data()
+        return tasking_object.retrieve_data()
 
 
 
