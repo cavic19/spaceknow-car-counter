1f8b9fc9a995e3d5fc9e473c251c43c9dc42040d
David <david.tomecek1@seznam.cz>
2021-10-29 05:35:48 +0200

    Modified setup

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='spackenow-car-counter',
+    name='spackenow-car-analyser',
     version='1.0',
     author='David Tomecek',
     author_email='david.tomecek1@seznam.cz',
-    packages=find_packages()
+    url='https://github.com/cavic19/spaceknow-car-counter',
+    install_requires=['Pillow','geojson','requests'],
+    packages=find_packages(exclude=['tests*']),
 )
